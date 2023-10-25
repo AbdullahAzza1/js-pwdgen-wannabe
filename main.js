7379dcdb190083e3bc7b820d47e65db0e92a21e9
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-25 16:42:35 +0200

    step1

---
+++ main.js
@@ -0,0 +1,8 @@
+// let nomeUtente = "abdullah";
+// let cognomeUtente = "azza";
+// let colorePreferito = "blu";
+
+
+const nomeUtente = prompt("inserisci il tuo nome");
+const cognomeUtente = prompt("inserisci il tuo cognome");
+const colorePreferito = prompt("inserisci il tuo colore preferito");
