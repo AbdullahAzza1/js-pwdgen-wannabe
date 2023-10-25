6d416ecdc584094c27f5f11eb63e4de5daf02230
AbdullahAzza1 <abdullahazza141@gmail.com>
2023-10-25 16:52:02 +0200

    step2

--- main.js
+++ main.js
@@ -6,3 +6,6 @@
 const nomeUtente = prompt("inserisci il tuo nome");
 const cognomeUtente = prompt("inserisci il tuo cognome");
 const colorePreferito = prompt("inserisci il tuo colore preferito");
+
+
+document.getElementById("titolo").innerHTML = "la tua password e" + nomeUtente;
